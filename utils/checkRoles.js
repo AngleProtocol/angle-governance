07a037f6ea0c79caa8f0e39ff20b78a966642460
0xtekgrinder <72015889+0xtekgrinder@users.noreply.github.com>
2024-02-09 09:57:44 -0500

    feat: check roles cron
    * fix: wrong lzEndpoint function name
    
    * chore: check-roles github action
    
    * feat: roles script that run every day and send a discord message
    
    * chore: add DISCORD_TOKEN to env in ci
    
    * feat: convert the check-roles message to embed
    
    * chore: specify the chain_ids to run the script
    
    * feat: don't post new messages unless something new is here
    
    * chore: use ci profile in check-roles ci
    
    * feat: update logs for a better understanding in checkRoles script
    
    * chore: use matrixes inside check-roles ci with run for each chain id
    
    * chore: compile inside check-roles ci
    
    * chore: add MAINNET uri
    
    * 
    
    * fix: filter out undefined embed message
    
    * chore: set parallel to 2
    
    * chore: install dependencies in check-roles ci
    
    * fix: check if the key is a decimal in checkScript
    
    * chore: try eth_node_uri_mainnet with llamarpc
    
    * chore: try to setup repo before
    
    * chore: update back to all chain ids
    
    * chore: set max parallel to 1
    
    * chore: retry forge script checkRoles
    
    * chore: set timeout for retries
    
    * chore: remove on PR run check-roles script

---
+++ utils/checkRoles.js
@@ -0,0 +1,88 @@
+const { readFileSync } = require("fs");
+const { Client, GatewayIntentBits, EmbedBuilder } = require("discord.js");
+
+const getChannel = (discordClient, channelName) => {
+  return discordClient.channels.cache.find(
+    (channel) => channel.name === channelName
+  );
+};
+
+const isDecimal = (s) => {
+  s = s.toString();
+  var regex = /^[0-9]*\.?[0-9]+$/;
+  return regex.test(s) && !isNaN(parseFloat(s)) && isFinite(s);
+}
+
+const rolesChannel = "roles-on-chain";
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
+});
+
+client.on("ready", async () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+
+  const channel = getChannel(client, rolesChannel);
+  if (!channel) {
+    console.log("discord channel not found");
+    return;
+  }
+
+  const content = readFileSync("./scripts/roles.json");
+  const roles = JSON.parse(content);
+  const chains = Object.keys(roles);
+  let embeds = [];
+  await Promise.all(
+    chains.map(async (chain) => {
+      const title = `⛓️ Chain: ${chain}`;
+      const keys = Object.keys(roles[chain]);
+      let message = "";
+      await Promise.all(
+        keys.map(async (key) => {
+          if (!isDecimal(key)) {
+            message += `\n👨‍🎤 **Actor: ${key}**\n`;
+            const actorKeys = Object.keys(roles[chain][key]);
+            await Promise.all(
+              actorKeys.map((actorKey) => {
+                message += `${roles[chain][key][actorKey].toString()}\n`;
+              })
+            );
+          } else {
+            message += `${roles[chain][key]}\n`;
+          }
+        })
+      );
+      embeds.push(new EmbedBuilder().setTitle(title).setDescription(message));
+    })
+  );
+
+  const lastMessages = (
+    await channel.messages.fetch({ limit: 20, sort: "timestamp" })
+  )
+    .map((m) => m.embeds)
+    .flat();
+  const latestChainIdsMessages = chains
+    .map((chain) => lastMessages.find((m) => m?.title == `⛓️ Chain: ${chain}`))
+    .filter((m) => m);
+  for (const embed of embeds) {
+    if (
+      latestChainIdsMessages.find(
+        (m) =>
+          m.data.description.replace(/\n/g, "") ==
+          embed.data.description.replace(/\n/g, "")
+      )
+    ) {
+      console.log("chain embed is already the same");
+      continue;
+    } else {
+      console.log(
+        "chain embed does not exist or isn't the same",
+        embed.data.title
+      );
+      await channel.send({ embeds: [embed] });
+    }
+  }
+  process.exit(0);
+});
+
+client.login(process.env.DISCORD_TOKEN);
